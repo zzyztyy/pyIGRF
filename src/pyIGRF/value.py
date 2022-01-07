71560d5f1b41b6c990440b717b1380c2b09bdede
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-01-07 23:27:12 +0100

    cleanup

--- src/pyIGRF/value.py
+++ src/pyIGRF/value.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import numpy as np
+from math import atan2, sqrt, pi
 
 from ._calculate import igrf12syn
+from ._typeguard import typechecked
 
-FACT = 180./np.pi
+
+FACT = 180.0 / pi
 
 
-def igrf_value(lat, lon, alt=0., year=2005.):
+@typechecked
+def igrf_value(
+    lat: float,
+    lon: float,
+    alt: float = 0.0,
+    year: float = 2005.0,
+) -> tuple[float, float, float, float, float, float, float]:
     """
     :return
          D is declination (+ve east)
@@ -18,14 +26,23 @@ def igrf_value(lat, lon, alt=0., year=2005.):
          Z is vertical component (+ve down)
          F is total intensity
     """
+
     x, y, z, f = igrf12syn(year, 1, alt, lat, lon)
-    d = FACT * np.arctan2(y, x)
-    h = np.sqrt(x * x + y * y)
-    i = FACT * np.arctan2(z, h)
+
+    d = FACT * atan2(y, x)
+    h = sqrt(x * x + y * y)
+    i = FACT * atan2(z, h)
+
     return d, i, h, x, y, z, f
 
 
-def igrf_variation(lat, lon, alt=0., year=2005):
+@typechecked
+def igrf_variation(
+    lat: float,
+    lon: float,
+    alt: float = 0.0,
+    year: float = 2005.0,
+) -> tuple[float, float, float, float, float, float, float]:
     """
          Annual variation
          D is declination (+ve east)
@@ -36,14 +53,16 @@ def igrf_variation(lat, lon, alt=0., year=2005):
          Z is vertical component (+ve down)
          F is total intensity
     """
-    x1, y1, z1, f1 = igrf12syn(year-1, 1, alt, lat, lon)
-    x2, y2, z2, f2 = igrf12syn(year+1, 1, alt, lat, lon)
-    x, y, z, f = (x1+x2)/2, (y1+y2)/2, (z1+z2)/2, (f1+f2)/2
-    dx, dy, dz, df = (x2-x1)/2, (y2-y1)/2, (z2-z1)/2, (f2-f1)/2
-    h = np.sqrt(x * x + y * y)
+
+    x1, y1, z1, f1 = igrf12syn(year - 1, 1, alt, lat, lon)
+    x2, y2, z2, f2 = igrf12syn(year + 1, 1, alt, lat, lon)
+    x, y, z, f = (x1 + x2) / 2, (y1 + y2) / 2, (z1 + z2) / 2, (f1 + f2) / 2
+    dx, dy, dz, df = (x2 - x1) / 2, (y2 - y1) / 2, (z2 - z1) / 2, (f2 - f1) / 2
+    h = sqrt(x * x + y * y)
 
     dd = (FACT * (x * dy - y * dx)) / (h * h)
     dh = (x * dx + y * dy) / h
     ds = (FACT * (h * dz - z * dh)) / (f * f)
     df = (h * dh + z * dz) / f
+
     return dd, ds, dh, dx, dy, dz, df

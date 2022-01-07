6f4ecce9c7164927994e25cba2628aa580d862b2
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-01-07 23:33:13 +0100

    style

--- setup.py
+++ setup.py
@@ -6,23 +6,27 @@ from setuptools import setup, find_packages
 SRC_DIR = "src"
 
 
+with open("README.md", mode = 'r', encoding = 'utf-8') as f:
+    readme = f.read()
+
+
 setup(
-    name="pyIGRF",
-    version="0.3.3",
-    author="zzyztyy",
-    author_email="2375672032@qq.com",
-    description="IGRF-13 Model by Python",
-    long_description=open("README.md").read(),
-    license="MIT",
-    url="https://github.com/zzyztyy/pyIGRF",
-    packages=find_packages(SRC_DIR),
-    package_dir={"": SRC_DIR},
-    install_requires=[],
-    extras_require={
+    name = "pyIGRF",
+    version = "0.3.3",
+    author = "zzyztyy",
+    author_email = "2375672032@qq.com",
+    description = "IGRF-13 Model by Python",
+    long_description = readme,
+    license = "MIT",
+    url = "https://github.com/zzyztyy/pyIGRF",
+    packages = find_packages(SRC_DIR),
+    package_dir = {"": SRC_DIR},
+    install_requires = [],
+    extras_require = {
         "dev": [
             "goto-statement",
             "typeguard",
         ]
     },
-    package_data={'': ['src/igrf13coeffs.txt']}
+    package_data = {'': ['src/igrf13coeffs.txt']}
 )

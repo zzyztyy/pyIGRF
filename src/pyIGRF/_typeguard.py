f05b56480457a6606ab55fd5d4ce57735e0132b3
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-01-07 22:41:46 +0100

    typeguard debug 2

---
+++ src/pyIGRF/_typeguard.py
@@ -0,0 +1,9 @@
+
+import os
+import warnings
+
+if os.environ.get('PYIGRF_DEBUG', '0') == '1':
+    from typeguard import typechecked
+    warnings.warn("running in debug mode with activated run-time type checks", RuntimeWarning)
+else:
+    typechecked = lambda x: x

3c60040f33f8f13f3ae28285485c0450e6963986
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-01-08 14:50:24 +0100

    prepare verify dataset

--- verificaton/verify.py
+++ verificaton/verify.py
@@ -5,11 +5,15 @@ from subprocess import Popen
 from typing import Union
 from urllib.request import urlopen, Request
 
+import h5py
+import numpy as np
 from pexpect import spawn
+from tqdm import tqdm
 from typeguard import typechecked
 
 
 CMD = "igrf13"
+DATA = "data.h5"
 FLD = os.path.dirname(__file__)
 URL = "https://www.ngdc.noaa.gov/IAGA/vmod/igrf13.f"
 
@@ -63,6 +67,45 @@ def _compute(
     proc.wait()
 
     return reply
+
+
+@typechecked
+def _compute_arrays(
+    fn: str,
+    year_step: float = 0.25,
+    lat_step: float = 0.5,
+    lon_step: float = 0.5,
+    alt_step: float = 10.0,
+):
+    """
+        'D': -0.8833333333333333,
+        'D_SV': 2.0,
+        'I': 13.333333333333334,
+        'I_SV': 7.0,
+        'H': 37151.0,
+        'H_SV': 43.0,
+        'X': 37144.0,
+        'X_SV': 43.0,
+        'Y': -723.0,
+        'Y_SV': 16.0,
+        'Z': 8804.0,
+        'Z_SV': 87.0,
+        'F': 38180.0,
+        'F_SV': 62.0,
+    """
+
+    years = np.arange(1900.0, 2030.0 + year_step, year_step, dtype = 'f8')
+    lat = np.arange(-90.0, 90.0 + lat_step, dtype = 'f8')
+    lon = np.arange(0.0, 360.0 + lon_step, dtype = 'f8')
+    alt = np.arange(-10.0, 400.0 + alt_step, alt_step, dtype = 'f8')
+
+    columns = ('D', 'I', 'H', 'X', 'Y', 'Z', 'F')
+    columns = columns + tuple(f'{column}_SV' for column in columns)
+
+    data = np.zeros(
+        (years.shape[0], lat.shape[0], lon.shape[0], alt.shape[0]),
+        dtype = 'f8',
+    )
 
 
 @typechecked
@@ -143,13 +186,11 @@ def main(clean: bool = False):
     if not os.path.exists(cmd_fn):
         _build(src_fn, cmd_fn)
 
-    ret = _compute(
-        year = 2020.456,
-        alt = 99.876,
-        lat = 12.345,
-        lon = 67.894,
-    )
-    print(ret)
+    data_fn = os.path.join(FLD, DATA)
+    if clean and os.path.exists(data_fn):
+        os.unlink(data_fn)
+    if not os.path.exists(data_fn):
+        _compute_arrays(data_fn)
 
 
 if __name__ == '__main__':

22c97ac03044964b6ce19abd6b62a1a36da46804
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-01-09 22:33:26 +0100

    benchmark runs

--- benchmark/scalar.py
+++ benchmark/scalar.py
@@ -5,7 +5,7 @@ import itertools
 from time import time_ns
 
 import numpy as np
-from tqdm import tqdm
+# from tqdm import tqdm
 from typeguard import typechecked
 
 from pyIGRF import get_syn
@@ -14,38 +14,49 @@ from pyIGRF import get_syn
 DTYPE = 'f4'
 
 
-@typechecked
-def main(
-    year_step: float = 0.5, # 0.5 ... 2.0
-    lat_step: float = 7.5, # 7.5 ... 90.0
-    lon_step: float = 7.5, # 7.5 ... 90.0
-    alt_step: float = 49.5, # 49.5 ... 100.0
-):
+def _single_run(
+    year: float,
+    iterations: int,
+    itype: int,
+) -> float:
 
-    years = np.arange(1900.0, 2030.0 + year_step, year_step, dtype = DTYPE)
-    lats = np.arange(-90.0, 90.0 + lat_step, lat_step, dtype = DTYPE)
-    lons = np.arange(0.0, 360.0, lon_step, dtype = DTYPE)
-    alts = np.arange(-100.0, 400.0 + alt_step, alt_step, dtype = DTYPE)
-    itypes = (1, 2) # above sea level, from centre of Earth
+    offset = 0.0 if itype == 1 else 6371.2
 
+    random = np.random.default_rng()
+
+    lats = [float(number) for number in random.uniform(low = -90.0, high = 90.0, size = iterations)]
+    lons = [float(number) for number in random.uniform(low = 0.0, high = 360.0, size = iterations)]
+    alts = [float(number) for number in random.uniform(low = -100.0, high = 400.0, size = iterations)]
+
+    gc.disable()
     start = time_ns()
 
-    for (year_idx, year), (lat_idx, lat), (lon_idx, lon), (alt_idx, alt), (itype_idx, itype) in tqdm(itertools.product(
-        enumerate(years), enumerate(lats), enumerate(lons), enumerate(alts), enumerate(itypes),
-    ), total = years.shape[0] * lats.shape[0] * lons.shape[0] * alts.shape[0] * len(itypes)):
-
-        offset = 0.0 if itype == 1 else 6371.2
+    for lat, lon, alt in zip(lats, lons, alts):
 
         _, _, _, _ = get_syn(
             year = year,
-            lat = float(lat),
-            elong = float(lon),
-            alt = float(alt) + offset,
+            lat = lat,
+            elong = lon,
+            alt = alt + offset,
             itype = itype,
         )
 
-    duration = (time_ns() - start) * 1e-9
-    print(f'duration={duration:f}s')
+    stop = time_ns()
+    gc.enable()
+
+    return (stop - start) * 1e-9
+
+
+@typechecked
+def main():
+
+    years = [1910.0, 1940.0, 1980.0, 2000.0, 2020.0, 2025.0]
+    iterations = [10 ** exp for exp in range(1, 6)]
+    itypes = (1, 2)
+
+    for year, iteration, itype in itertools.product(years, iterations, itypes):
+        duration = _single_run(year = year, iterations = iteration, itype = itype)
+        print(f'year={year:f} iteration={iteration:f} itype={itype:d} duration={duration:f}s')
 
 
 if __name__ == '__main__':

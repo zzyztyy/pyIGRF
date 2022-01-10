95a1cd04929c94a327e6327f1bc7537944d579ed
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-01-10 19:36:12 +0100

    array implementation, starting with jit implementation copy

---
+++ src/pyIGRF/array/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+Array implementation
+"""
+
+from ._calculate import (
+    geodetic2geocentric,
+    get_syn,
+)
+
+from ._coeffs import (
+    get_coeffs,
+)
+
+from ._value import (
+    get_value,
+    get_variation,
+)

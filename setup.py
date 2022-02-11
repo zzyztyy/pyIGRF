367e10921797509b59417c4d53f0308c07c706e0
Sebastian M. Ernst <ernst@pleiszenburg.de>
2022-02-11 15:48:49 +0100

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", mode = 'r', encoding = 'utf-8') as f:
 
 setup(
     name = "pyIGRF",
-    version = "0.3.4",
+    version = "0.4.0",
     author = "pyIGRF authors",
     author_email = "ernst@pleiszenburg.de",
     description = "IGRF-13 Model in Python",
